48f75d90eb8b02de085e8e0159a3e72d81bbe771
Alex Ramos <alex.ramos@kellogg.northwestern.edu>
2024-01-25 00:55:55 +0000

    yup

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -17,3 +17,22 @@
 # Combine strings and variables
 
 # String manipulation
+
+# put the number 2 on screen
+puts 2
+
+# put math on screen. it does math!
+
+puts 2+5
+
+# put string on screen
+puts "Turn my swag on"
+
+#define variables
+x=5
+food="Burritos"
+
+puts food
+
+#concatenate stuff
+puts "#{x}...#{}"
